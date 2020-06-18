d04413a1344ff4045c3cba7132321bc723f7c674
camelya58 <camelya@mail.ru>
2020-06-18 13:40:58 +0300

    add main class

---
+++ src/main/java/com/github58/camelya/ubuntu/UbuntuApplication.java
@@ -0,0 +1,13 @@
+package com.github58.camelya.ubuntu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbuntuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbuntuApplication.class, args);
+    }
+
+}
